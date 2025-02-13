df58d9b3807dc722668703267b8cd365d411c5b0
Jan Plastun <undermed@bk.ru>
2025-02-13 23:14:48 +0300

    fix_bug.006

--- js/modal.js
+++ js/modal.js
@@ -2,13 +2,19 @@
 let modalCall = document.getElementById('modalCall');
 let call = document.getElementById('call');
 let call2 = document.getElementById('call2');
+let call3 = document.getElementById('call3');
 let closeCall = document.getElementById('modalClose');
 let backgruound = document.querySelector('.backgruoundAll')
 
-let openCallModal = call.onclick = call2.onclick = function() {
+let closeMenu = function() {
+  menu.style.display = 'none';
+}
+
+let openCallModal = call.onclick = call2.onclick = call3.onclick = function() {
   if (modalCall.style.display === 'none'|| modalCall.style.display === '') {
     modalCall.style.display = 'block';
     backgruound.classList.add('modal-active');
+    closeMenu()
   }
   else {
     modalCall.style.display = 'none';
@@ -25,13 +31,15 @@ document.addEventListener('keydown', function(event) {
 });
 let modalFeedback = document.getElementById('modalFeedback');
 let feedback = document.getElementById('feedbackOpen');
+let feedback2 = document.getElementById('feedbackOpen2');
+let feedback3 = document.getElementById('feedbackOpen3');
 let closeFeedback = document.getElementById('modalCloseFeedback');
-let feedback2 =document.getElementById('feedbackOpen2');
 
-let openFeedbackModal = feedback.onclick = feedback2.onclick = function() {
+let openFeedbackModal = feedback.onclick = feedback2.onclick = feedback3.onclick = function() {
   if (modalFeedback.style.display === 'none'|| modalFeedback.style.display === '') {
     modalFeedback.style.display = 'block';
     backgruound.classList.add('modal-active');
+    closeMenu()
   }
   else {
     modalFeedback.style.display = 'none';
